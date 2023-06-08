6004e9438a09201b4f87a6f7d9af4feaec51b244
Tangela <thetengrape@163.com>
2023-06-08 13:35:08 +0800

    feat: impl basic SDK

--- lib/main.ts
+++ lib/main.ts
@@ -1,5 +1,84 @@
-import { getIpfsPath } from "./request";
+import { createRequestClient, defaultClient } from './request'
 
-(window as any).getIpfsPath = getIpfsPath;
+interface Config {
+  baseURL: string
+}
+interface ProgressEvent {
+  percent?: number
+}
+interface Hooks {
+  onProgress?: (event: ProgressEvent) => void
+  onSuccess?: (body: any) => void
+  onError?: (error: Error) => void
+}
 
-export { getIpfsPath };
+const getTs = () => Math.floor(Date.now() / 1000).toString()
+
+export class Client {
+  apiClient: ReturnType<typeof createRequestClient>
+  config: Config
+
+  constructor(public key: string, public secret: string, config?: Config) {
+    this.key = key
+    this.secret = secret
+    this.config = config || {
+      baseURL: 'http://starbase.gw3.io',
+    }
+
+    this.apiClient = createRequestClient(this.config.baseURL, this.key, this.secret)
+  }
+
+  async uploadFile(file: File, hooks?: Hooks) {
+    try {
+      const url = await this._getIpfsPath(file.size)
+
+      const { headers } = await defaultClient.post(url, file, {
+        onUploadProgress: ({ loaded, total }) => {
+          if (total) {
+            const percent = Math.round((loaded / total) * 100)
+
+            hooks?.onProgress?.({ percent: percent >= 100 ? 99 : percent })
+          }
+        },
+      })
+      const cid = headers['ipfs-hash']
+
+      hooks?.onProgress?.({ percent: 100 })
+      hooks?.onSuccess?.(cid)
+
+      return cid
+    } catch (error: any) {
+      hooks?.onError?.(error)
+    }
+  }
+
+  async getIpfs(cid: string) {
+    const url = `/ipfs/${cid}&ts=${getTs()}`
+    const { data } = await this.apiClient.get(url)
+    return data
+  }
+
+  async addPin(cid: string) {
+    const pinUrl = `/api/v0/pin/add?arg=${cid}&ts=${getTs()}`
+    const { data } = await this.apiClient.post(pinUrl)
+    return data
+  }
+
+  async removePin(cid: string) {
+    const pinUrl = `/api/v0/pin/rm?arg=${cid}&ts=${getTs()}`
+    const { data } = await this.apiClient.post(pinUrl)
+    return data
+  }
+
+  async _getIpfsPath(size: number) {
+    const ipfsUrl = `/ipfs/?size=${size}&ts=${getTs()}`
+
+    const {
+      data: { url },
+    } = await this.apiClient.post(ipfsUrl)
+
+    return url
+  }
+}
+
+console.log(new Client('key', 'secret'), Client)
--- lib/request.ts
+++ lib/request.ts
@@ -1,42 +1,22 @@
-import axios from "axios";
-import type { IsEqual, SetRequired } from "type-fest";
+import axios from 'axios'
+import { sign } from './utils'
 
-import { sign } from "./utils";
+export const defaultClient = axios
 
-type TRes<T> = {
-  code: number;
-  msg?: string;
-  data?: T;
-};
-export type PhotonRes<T = any> = IsEqual<T, any> extends true
-  ? TRes<T>
-  : SetRequired<TRes<T>, "data">;
-
-export interface PhotonListRes<T = any> {
-  code: number;
-  msg?: string;
-  data: T[];
-  total: number;
-}
-
-const timeout = 1000 * 60 * 5;
-
-export function getIpfsPath(
-  baseURL: string,
-  size: number,
-  key: string,
-  secret: string
-) {
-  const ts = Math.floor(Date.now() / 1000).toString()
-  console.log(ts)
-  const url = `/ipfs/?size=${size}&ts=${ts}`;
-
-  return axios.post(url, undefined, {
+export const createRequestClient = (baseURL: string, key: string, secret: string) => {
+  const apiClient = axios.create({
     baseURL,
-    headers: {
-      "X-Access-Key": key,
-      "X-Access-Signature": sign("POST", url, secret),
-    },
-    timeout,
-  });
+    timeout: 1000 * 60 * 5,
+  })
+
+  apiClient.interceptors.request.use((config) => {
+    config.headers = {
+      'X-Access-Key': key,
+      'X-Access-Signature': sign('POST', config.url!, secret),
+    } as any
+
+    return config
+  })
+
+  return apiClient
 }
